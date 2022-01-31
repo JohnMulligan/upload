c2a66ab82071cd5077e516fbd571bd80189bb63d
Jingwen Hu <jinghuw@gmail.com>
2022-01-31 01:34:24 -0600

    pod

--- ios/upload/AppDelegate.m
+++ ios/upload/AppDelegate.m
@@ -97,7 +97,7 @@ static void InitializeFlipper(UIApplication *application) {
 }
 
 // Linking API
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+- (BOOL)application:(UIApplication *_Nonnull)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *_Nonnull)options {
   return [RCTLinkingManager application:application openURL:url options:options];
 }
 
